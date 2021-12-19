617451171a50d82a6f4b92093f7594cffb534536
Terry Chen <chenterry85@gmail.com>
2021-12-19 14:46:11 -0800

    Add env variables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv')
 
 const app = express();
 
@@ -7,5 +8,9 @@ const app = express();
 // Connect to MongoDB
 main().catch(err => console.error(err));
 async function main(){
-  await mongoose.connect('');
+  await mongoose.connect(process.env.MONGODB_URI);
 }
+
+// Listen
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
+
