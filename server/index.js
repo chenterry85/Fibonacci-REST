aa7259398af18a148b68ad472c34629df19af7ac
Terry Chen <chenterry85@gmail.com>
2021-12-19 19:50:30 -0800

    Prepare SPA

--- server/index.js
+++ server/index.js
@@ -9,6 +9,17 @@ const app = express();
 app.use( express.json() );
 app.use( cors() );
 
+// Handle Production
+if(process.env.NODE_ENV === 'production'){
+  // static folder
+  app.use(express.static(__dirname + '/public'));
+
+  // Handle Single Page Application
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + 'public/index.html');
+  })
+}
+
 // Setup routes
 const fibsRoute = require('./routes/fibs');
 app.use('/api/fibs', fibsRoute);
